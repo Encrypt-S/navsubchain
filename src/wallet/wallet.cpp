b84cc8952db6dcf9ccb4ba623588144e54500abf
alex v <alexvazquezmusic@me.com>
2017-12-21 12:54:49 +0100

    Update wallet.cpp

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2641,8 +2641,10 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
 
     if(wtxNew.nCustomVersion > 0) txNew.nVersion = wtxNew.nCustomVersion;
 
-    txNew.strDZeel = wtxNew.strDZeel.length() > 0 ? wtxNew.strDZeel : SUBtech.EncryptAddress(std::to_string(GetAdjustedTime() + (rand() % 1<<8)),sPubKey);
-
+    txNew.strDZeel = (wtxNew.strDZeel != "" ? wtxNew.strDZeel : strDZeel).length() > 0 ?
+                (wtxNew.strDZeel != "" ? wtxNew.strDZeel : strDZeel) :
+                navtech.EncryptAddress(std::to_string(GetAdjustedTime() + (rand() % 1<<8)),sPubKey);
+    
     if (strDZeel.length() > 0)
       wtxNew.fAnon = true;
 
